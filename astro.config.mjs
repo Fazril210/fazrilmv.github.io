85189d8b490a6ac39da456d7f905396a9bffd458
Fazril Mantovani <fazrilmantovani8@gmail.com>
2026-08-12 00:57:59 +0700

    chore: update base path for fazrilmv.github.io repository

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://fazril210.github.io',
-  base: '/portofolio_web',
+  base: '/',
 });
